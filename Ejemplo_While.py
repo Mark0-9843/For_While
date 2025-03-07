d56114b482a354f28444d1c97cf9709e544decf2
Marko <zS23013998@estudiantes.uv.mx>
2025-03-07 17:09:48 -0600

    Add files via upload

---
+++ Ejemplo_While.py
@@ -0,0 +1,10 @@
+# Marco Antonio Morales González
+
+# ESTE CODIGO IMPRIME LOS NUMEROS DEL 100 AL 0 USANDO WHILE
+
+# 07 / 03 / 2025 - V. 1. 0. 0 - EJEMPLO WHILE
+
+numero = 101        #Creamos la variable que cambiará a través del While
+while numero > 0:   #Creamos la estructura While que funcionará hasta que numero sea menor que 0
+    numero -= 1     #Restamos 1 a la variable numero
+    print(numero)   #Imprimimos la variable numero
